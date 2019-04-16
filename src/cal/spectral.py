fd5692055ba82c68a16157e2968bf04a062c8a0d
Brian-Szutu <44279339+Brian-Szutu@users.noreply.github.com>
2019-04-16 13:38:13 -0700

    Does spectral band math to radiance images

---
+++ src/cal/spectral.py
@@ -0,0 +1,205 @@
+"""
+Authors: Brian Szutu
+Emails: bs886@nau.edu
+License: Northern Arizona University
+Copyright: 2018-2019
+
+This script performs band math on each rad.tif image using the outputted
+average atmospheric correction values in the .txt outputted by atmcorr_regr.py.
+The band math is as follows:
+s1 - s2.
+s1 is the band data at a certain band between bands 1 and 7, inclusive.
+s2 is the corresponding average atmospheric correction value for that
+specific band.
+
+The spectral-mathed image will be outputted in the same folder as the
+radiance image.
+
+The script should be called from console and the inputted directory should
+contain all of the folders containing the images.
+"""
+
+import argparse
+import os
+import rasterio
+
+def args_parser():
+    """
+    Reads the image directory passed in from console.
+
+    Parameters:
+    None
+
+    Return:
+    Returns a directory that has the images to be analyzed within it
+    """
+
+    # Creates an ArgumentParser object to hold the console input
+    parser = argparse.ArgumentParser()
+    
+    # Adds an argument to the parser created above. Holds the
+    # inputted directory as a string
+    parser.add_argument('-ip', '--input_dir', type=str,
+                        help='The directory containing the images.')
+
+    # Returns the passed in directory
+    return parser.parse_args().input_dir
+
+
+def avgs_finder(atmotxt_dir):
+    """
+    Finds the average atmospheric correction values for bands 1 to 7.
+    Returns them as a list
+
+    Parameters:
+    atmotxt_dir - the directory of the .txt file with the average
+                  atmospheric correction values
+
+    Return:
+    A list containing the average atmospheric correction values of
+    bands 1 through 7
+    """
+
+    # Initializes an empty list to store the average correction
+    # values
+    averages = []
+
+    # Opens the .txt document 
+    with open(atmotxt_dir, 'r') as atmo_txt:
+        # Stores each line as its own list inside a larger list
+        # Splitting is done due to the formatting in the file
+        text = [line.strip().replace("\n", "").split(": ")
+                    for line in atmo_txt]
+    # Closes the file
+    atmo_txt.close()
+
+    # Sets a counter variable
+    n = -7
+    
+    # while loop goes through the last seven lines of the saved
+    # .txt file
+    while n < 0:
+        # Appends the averages to the initialized averages list
+        # Row n, column 1 due to the splitting from earlier
+        averages.append(float(text[n][1]))
+        
+        # Adds 1 to the counter
+        n += 1
+
+    return averages
+
+
+def spec_mather(rad_dir, averages, folder_dir):
+    """
+    Does the spectral band math to the image. A new image is created
+    as a result, with its name being the name of the rad.tif image but
+    with _specmath after rad. The band math is as follows:
+    s1 - s2.
+    s1 is the band data of one of the bands of the original rad.tif image
+    while s2 is the corresponding average band atmospheric correction value
+
+    Parameters:
+    rad_dir    - the directory of the rad.tif image
+    averages   - a list holding the average atmospheric correction values of
+                 bands 1 through 7
+    folder_dir - the directory of the image's folder
+    """
+
+    # Opens the rad.tif image
+    src = rasterio.open(rad_dir)
+
+    # Gets the metadata of the image
+    meta = src.meta
+
+    # Creates the specmath.tif image to be written onto
+    with rasterio.open(rad_dir.replace('.tif', '_specmath.tif'),
+                       'w', **meta) as dst:
+        # for bands 1 through 7...
+        for i in range(7):
+            # Calculate the band-mathed value
+            spec = src.read(i+1) - averages[i]
+            # and write it into the new image
+            dst.write(i+1, spec)
+
+    # Close the file
+    dst.close
+
+
+def main():
+    """
+    The main function. Calls the other functions.
+
+    Paramters:
+    None
+
+    Return:
+    None
+    """
+
+    # Gets the working directory from the console input
+    working_dir = args_parser()
+
+    # Initializes an empty list to hold all of the relevant folders
+    # in the specified directory
+    folders = []
+
+    # for each file in the specified directory...
+    for file in os.listdir(working_dir):
+        # if the file is NOT a folder
+        if "." in file:
+            continue
+        # if the file is a folder...
+        else:
+            # append it to the folders list
+            folders.append(file)
+
+    # for each folder in the folders list...
+    for folder in folders:
+        # Initialize variables to hold the atmcorr_regr.py output .txt and
+        # the rad.tif image.
+        avg_txt = ''
+        rad_file = ''
+
+        # Saves the directory of the folder
+        folder_dir = os.path.join(working_dir, folder)
+
+        # For each file in the folder...
+        for file in os.listdir(folder_dir):
+            # if the file is a rad.tif image that doesn't contain
+            # P1BS, save it
+            if file.endswith('rad.tif') and ('P1BS') not in file:
+                rad_file = file
+            # if the file is the output .txt from atmcorr_regr.py,
+            # save it
+            elif file == folder + '.txt':
+                avg_txt = file
+            # else continue
+            else:
+                continue
+        # Checks to see if the specmath.tif image exists
+        specmath_file_exists = os.path.isfile(os.path.join(folder_dir,
+                                                           rad_file.replace('.tif', '_specmath.tif')))
+
+        # If the specmath.tif image doesn't exist, create it
+        if not specmath_file_exists:
+            # Saves the directory of the atmcorr_regr.py file
+            atmotxt_dir = os.path.join(folder_dir, folder + '.txt')
+            # Calls avgs_finder to retrieve the averages from the file
+            averages = avgs_finder(atmotxt_dir)
+            # Saves the directory of the rad.tif image
+            rad_dir = os.path.join(folder_dir, rad_file)
+            # Calls spec_mather to do the band math and write
+            # it to the new file
+            spec_mather(rad_dir, averages, folder_dir)
+
+            print(rad_file + ' has been band-mathed!')
+
+        elif avg_txt == '':
+            print('atmcorr_regr.py has not been run yet in the directory.')
+
+        elif rad_file == '':
+            print('The radiance image does not exist in ' +
+                  folder_dir + '!')
+            
+if __name__ == '__main__':
+    main()
