dd4d94462357ddb50456879370450801aa04fa28
Brian-Szutu <44279339+Brian-Szutu@users.noreply.github.com>
2020-10-14 22:13:14 -0700

    Update endmembers.py
    Added the ability to read from an already existing downsampled PRR dataset.
    Added the endmember extraction section

--- src/classification/endmembers.py
+++ src/classification/endmembers.py
@@ -17,14 +17,15 @@ This script is safe to run multiple times in the same directory.
 import os
 import argparse
 import numpy as np
+import random
 import rasterio
 import pysptools.abundance_maps.amaps as amaps
-from matplotlib import pyplot as plt
 
 
 def args_parser():
     """
-    Reads the image directory passed in from console.
+    Reads the image directory passed in from console. Also reads in the
+    number of runs the endmember extractor should do.
     Parameters:
     None
     Return:
@@ -38,13 +39,152 @@ def args_parser():
     # inputted directory as a string
     parser.add_argument('-ip', '--input_dir', type=str,
                         help='The directory containing the images.')
+    parser.add_argument('-run_n', '--run_count', nargs='?',
+                        const=500, default=500, type=int,
+                        help='The number of runs to be done by the'
+                        +' endmember extractor.')
 
     # Returns the passed in directory
-    return parser.parse_args().input_dir
+    return (parser.parse_args().input_dir, parser.parse_args().run_count)
 
-def endmember_finder():
+def endmember_reader(working_dir):
+    """
+    Reads the .txt file containing the possible geologic endmembers. 
+    Parameters:
+    working_dir - the directory that was passed in from the console
+    Return:
+    Returns two arrays. The first contains the sample ID, lithology type,
+    and whether the sample was from a specimen's surface or interior. The
+    second array contains the eight bands that the spectral data was
+    downsampled to.
+    """
+    
+    working_dir = ('C:/Users/Brian/Documents/Salvatore Research/' +
+                   'WIP/polar_rock_repo_multispectral_WV02.txt')
 
-    return a
+    # Creates two lists to store the descriptions of each sample and the
+    # eight bands downsampled to, respectively
+    name_arr = []
+    spect_arr = []
+
+    # A temporary list to store the text from the document
+    temp_arr = []
+
+    # Opens the .txt file containing the possible endmembers
+    with open(working_dir, 'r') as endmem_file:
+        # and stores the text into the temporary list
+        temp_arr = [line.strip().split() for line in endmem_file]
+
+    # Closes the file
+    endmem_file.close()
+
+    # Stores the descriptions into the appropriate list
+    name_arr = temp_arr[0:2]
+    # Lops off the extraneous information (what each descriptor is)
+    for i in range(len(name_arr)):
+        name_arr[i] = name_arr[i][1:]
+
+    # Converts the list to a numpy array
+    name_arr = np.array(name_arr, dtype='str')
+    # Transposes the array to more easily access the information
+    # through indexing
+    name_arr = name_arr.transpose(1, 0)
+
+    # Stores the each sample's eight bands
+    spect_arr = temp_arr[3:]
+    # Lops off the extraneous information (what each band corresponds to)
+    for i in range(len(spect_arr)):
+        spect_arr[i] = spect_arr[i][1:]
+
+    # Converts the list to a numpy array
+    spect_arr = np.array(spect_arr, dtype='float')
+    # Transposes the array to match the pysptools input format
+    spect_arr = spect_arr.transpose(1, 0)
+
+    # Returns the two arrays
+    return (name_arr, spect_arr)
+
+def endmember_finder(band_data, spect_arr, run_count):
+    """
+    Reads the .txt file containing the possible geologic endmembers. 
+    Parameters:
+    band_data - an array containing the band data collected from satellite
+                imagery. pixel x band
+    spect_arr - an array containing the band data collected from ground
+                samples. endmember x band
+    run_count - the number of times to run the pysptools endmember
+                extraction function, with each time using a different
+                set of 8 endmembers
+    Return:
+    Returns two arrays. The first contains the abundances of the endmembers
+    in each run. run x endmember abundance. The second contains the indices
+    of those endmembers. run x endmember
+    """
+
+    # Keeps track of the sample used in each endmember extraction run
+    index_list = []
+
+    # Keeps track of the abundances in each run
+    abundances = []
+
+    # In each run...
+    for i in range(run_count):
+        # A temporary list to store the indices of samples
+        temp_list = []
+
+        # While there aren't eight endmembers to use in the endmember
+        # extraction function...
+        while len(temp_list) != 8:
+            # Get a random endmember...
+            rand_n = random.randrange(0, len(spect_arr))
+
+            # And if the endmember wasn't already picked...
+            if rand_n not in temp_list:
+                # Save it to be used
+                temp_list.append(rand_n)
+
+            # If the endmember list matches a previously used one...
+            if len(temp_list) == 8 and temp_list in index_list:
+                # Empty the list and start over
+                temp_list = []
+
+        # A temporary list to store the band data of the chosen samples
+        temp_endm = []
+        # For each sample, store the band data in the list
+        for index in temp_list:
+            temp_endm.append(spect_arr[index])
+
+        # Turns the list into a numpy array to be put into the pysptools
+        # function
+        temp_endm = np.array(temp_endm)
+
+        # Stores the indices into the index list
+        index_list.append(temp_list)
+
+        # Gets the abundances from this particular set of endmembers
+        temp_abun = amaps.NNLS(band_data, temp_endm)
+        # Stores the abundances in the list
+        abundances.append(temp_abun)
+
+    """
+    Visualization stuff. Unblock if band 1 comparison of the first pixel is
+    needed between the actual data and the sum of extracted endmembers
+    print(abundances[0][0])
+    print(band_data[0])
+    new = []
+    for i in index_list:
+        new.append(spect_arr[i])
+    new = np.array(new)
+    print(new)
+    der = new.transpose()[0].transpose() * abundances[0][0]
+    print(der)
+    print(der.sum())
+    input()
+    """
+
+    # Returns the abundance and index lists
+    return (abundance, index_list)
+
 
 def band_extractor(image_dir):
     """
@@ -97,12 +237,12 @@ def band_extractor(image_dir):
     return (band_data, image_dim)
 
 
-
 def main():
     
-    working_dir = args_parser()
+    #(working_dir, run_count) = args_parser()
 
-    # working_dir = 'C:/Users/Brian/Documents/Salvatore Research/test_image'
+    working_dir = 'C:/Users/Brian/Documents/Salvatore Research/test_image'
+    run_count = 1
 
     # A list that contains all of the "_class" .tif files
     class_files = []
@@ -116,7 +256,12 @@ def main():
         else:
             continue
 
+    # If the class file does not exist...
     if class_count != 0:
+
+        # Extract the downsampled PRR samples as potential endmembers
+        (name_arr, spect_arr) = endmember_reader(working_dir)
+        
         # For each detected _class.tif image file...
         for image in class_files:
             endmember_exist = os.path.isfile(
@@ -127,6 +272,8 @@ def main():
                 # The file address of the image
                 image_dir = os.path.join(working_dir, image)
                 (band_data, image_dim) = band_extractor(image_dir)
+                (abundance, index_list) = endmember_finder(band_data, spect_arr,
+                                                           run_count)
                 
 
             else:
